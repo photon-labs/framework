779335dad5ea5831ec3e6de5814615c5b3f5ab0d
sureshkumar_ra <sureshkumar.rajendran@photoninfotech.net>
2012-09-14 15:09:58 +0530

    [artf533036] In Framework - Loading icon commonly moved to baselayout page

--- phresco-framework-web/src/main/webapp/js/phresco/common.js
+++ phresco-framework-web/src/main/webapp/js/phresco/common.js
@@ -43,7 +43,7 @@ function disableBtn(disableId) {
 // This function to enable the screen
 function enableScreen() {
     $(".wel_come").show().css("display", "none");
-    $("#loadingIconDiv").empty();
+    $("#loadingIconDiv").hide();
 }
 
 // This function to disable the screen
@@ -61,15 +61,16 @@ function showProgessBar(dispData, time) {
 }
 
 // Loading icon    
-function showLoadingIcon(tagControl) {
-	var src = "themes/photon/images/loading_blue.gif";
-	var theme =localStorage["color"];
-    if (theme == undefined || theme == null || theme == "null" || theme == "" || 
-    		theme == "undefined" || theme == "themes/photon/css/red.css") {
-    	src = "themes/photon/images/loading_red.gif";
-    }
- 	tagControl.empty();
-	tagControl.html("<img class='loadingIcon' src='"+ src +"' style='display: block'>");
+function showLoadingIcon() {
+    	disableScreen();
+		var src = "themes/photon/images/loading_blue.gif";
+    	var theme =localStorage["color"];
+        if (theme == undefined || theme == null || theme == "null" || theme == "" || 
+        		theme == "undefined" || theme == "themes/photon/css/red.css") {
+        	src = "themes/photon/images/loading_red.gif";
+        }
+        $("#loadingIconDiv").show();
+    	$("#loadingIconImg").attr("src", src);
 }
 
 //hide the progress bar
