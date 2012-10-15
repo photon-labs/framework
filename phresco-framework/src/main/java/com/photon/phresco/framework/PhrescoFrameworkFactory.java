c3ceb530344bdb62e06e2acd9b220c6af5328b43
kumar_s <kumar.selvaraj@photoninfotech.net>
2012-10-15 20:50:33 +0530

    ConfigManager.java moved to phresco commons

--- phresco-framework/src/main/java/com/photon/phresco/framework/PhrescoFrameworkFactory.java
+++ phresco-framework/src/main/java/com/photon/phresco/framework/PhrescoFrameworkFactory.java
@@ -23,10 +23,10 @@ import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+import com.photon.phresco.commons.api.ConfigManager;
 import com.photon.phresco.exception.PhrescoException;
 import com.photon.phresco.framework.api.ApplicationManager;
 import com.photon.phresco.framework.api.CIManager;
-import com.photon.phresco.framework.api.ConfigManager;
 import com.photon.phresco.framework.api.ProjectAdministrator;
 import com.photon.phresco.framework.api.ProjectManager;
 import com.photon.phresco.framework.api.ProjectRuntimeManager;
