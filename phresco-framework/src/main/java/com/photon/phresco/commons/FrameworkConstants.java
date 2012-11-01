ef5b967bf36f4b835fa7996b72c25c48bf6c26b6
suresh-ma <suresh.marimuthu@photoninfotech.net>
2012-11-01 17:02:00 +0530

    constant changed

--- phresco-framework/src/main/java/com/photon/phresco/commons/FrameworkConstants.java
+++ phresco-framework/src/main/java/com/photon/phresco/commons/FrameworkConstants.java
@@ -1546,13 +1546,11 @@ public interface FrameworkConstants {
 		/**
 		 * Test and test report directory constants
 		 */
-		String POM_PROP_KEY_UNITTEST_DIR = "phresco.unitTest.dir";
 		String POM_PROP_KEY_UNITTEST_RPT_DIR = "phresco.unitTestReport.dir";
 		String POM_PROP_KEY_UNITTEST_TESTSUITE_XPATH = "phresco.unitTest.testsuite.xpath";
 		String POM_PROP_KEY_UNITTEST_TESTCASE_PATH = "phresco.unitTest.testcase.path";
 		String POM_PROP_KEY_LOADTEST_DIR = "phresco.loadTest.dir";
 		String POM_PROP_KEY_LOADTEST_RPT_DIR = "phresco.loadTestReport.dir";
-		String POM_PROP_KEY_FUNCTEST_DIR = "phresco.functioanlTest.dir";
         String POM_PROP_KEY_FUNCTEST_RPT_DIR = "phresco.functionalTestReport.dir";
         String POM_PROP_KEY_FUNCTEST_TESTSUITE_XPATH = "phresco.functionalTest.testsuite.xpath";
         String POM_PROP_KEY_FUNCTEST_TESTCASE_PATH = "phresco.functionalTest.testcase.path";
