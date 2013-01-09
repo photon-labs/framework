e8ed88c2ca2cb6a5a590412240cc266cc6d7dcb6
kumar_s <kumar.selvaraj@photoninfotech.net>
2013-01-09 20:45:58 +0530

    issue fixed in downloads page as some of downloads size coming as null

--- phresco-framework/src/main/java/com/photon/phresco/commons/FrameworkConstants.java
+++ phresco-framework/src/main/java/com/photon/phresco/commons/FrameworkConstants.java
@@ -110,6 +110,7 @@ public interface FrameworkConstants {
     String REQ_FEATURES_TYPE = "type";
     String REQ_FEATURES_MOD_GRP = "moduleGroup";
     String MEGABYTE = "MB";
+    String KILOBYTE = "KB";
     
     String REQ_KEY = "key";
     String REQ_VALUE = "value";
