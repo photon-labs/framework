eef9bef167caad788022c1692f5b607c20364913
kaleeswaran <kaleeswaran.subbaiya@photoninfotech.net>
2013-01-24 20:53:53 +0530

    Windows metro technology CI failure

--- phresco-framework/src/main/java/com/photon/phresco/framework/model/CIJob.java
+++ phresco-framework/src/main/java/com/photon/phresco/framework/model/CIJob.java
@@ -117,7 +117,9 @@ public class CIJob {
     private String theme = "";
     private String deviceKeyPassword = "";
     private String emulatorKeyPassword = "";
-    
+    private String type = "";
+    private String platform = "";
+
     public CIJob() {
         super();
     }
@@ -713,5 +715,21 @@ public class CIJob {
 
 	public void setEmulatorKeyPassword(String emulatorKeyPassword) {
 		this.emulatorKeyPassword = emulatorKeyPassword;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
 	}
 }
