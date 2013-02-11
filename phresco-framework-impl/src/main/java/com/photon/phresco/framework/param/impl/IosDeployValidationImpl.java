0fd61c6612a1f524c6540d99cda0157bbccb8f4b
kaleeswaran <kaleeswaran.subbaiya@photoninfotech.net>
2013-02-11 14:46:06 +0530

    Deploy to device through CI issue fixed.

--- phresco-framework-impl/src/main/java/com/photon/phresco/framework/param/impl/IosDeployValidationImpl.java
+++ phresco-framework-impl/src/main/java/com/photon/phresco/framework/param/impl/IosDeployValidationImpl.java
@@ -12,14 +12,18 @@ import org.xml.sax.*;
 import com.photon.phresco.api.*;
 import com.photon.phresco.commons.model.*;
 import com.photon.phresco.exception.*;
+import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.*;
 import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues;
 import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues.*;
+import com.photon.phresco.plugins.util.*;
 import com.photon.phresco.util.*;
 
 public class IosDeployValidationImpl implements DynamicParameter {
 	private String BUILD_INFO_FILE_NAME = "build.info";
 	private String DO_NOT_CHECKIN_DIR = "do_not_checkin";
 	private String BUILD = "build";
+    private String FALSE = "false";
+	private String TRUE = "true";
 	
 	public PossibleValues getValues(Map<String, Object> paramsMap)
 			throws IOException, ParserConfigurationException, SAXException,
@@ -47,11 +51,15 @@ public class IosDeployValidationImpl implements DynamicParameter {
                     value.setValue("simulator");
                     value.setDependency("sdkVersion,family,logs,buildNumber");
                     possibleValues.getValue().add(value);
+                    // set trigger simulator value
+                    setTriggerSimulatorValue(paramsMap, TRUE);
                     return possibleValues;
             	} else { // if it is device, it should return null and should not show any popup
             		Value value = new Value();
                     value.setValue("device");
                     possibleValues.getValue().add(value);
+                    // set trigger simulator value
+                    setTriggerSimulatorValue(paramsMap, FALSE);
             		return possibleValues;
             	}
             }
@@ -62,6 +70,14 @@ public class IosDeployValidationImpl implements DynamicParameter {
 		return possibleValues;
 	}
 
+	private void setTriggerSimulatorValue(Map<String, Object> paramsMap, String triggerSimulator) throws PhrescoException {
+    	MojoProcessor mojo = (MojoProcessor) paramsMap.get(KEY_MOJO);
+    	String goal = (String) paramsMap.get(KEY_GOAL);
+    	Parameter parameter = mojo.getParameter(goal, KEY_TRIGGER_SIMULATOR);
+		parameter.setValue(triggerSimulator);
+    	mojo.save();
+	}
+	
 	private StringBuilder getBuildInfoPath(String projectDirectory) {
 	    StringBuilder builder = new StringBuilder(Utility.getProjectHome());
 	    builder.append(projectDirectory);
