a3cf2b858ded0b1ffbd5449097aa8f4248ab4c5f
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2013-02-14 00:03:47 +0530

    ios-sim added

--- phresco-framework-runner/delivery/bin/env.sh
+++ phresco-framework-runner/delivery/bin/env.sh
@@ -4,5 +4,6 @@ export M2_HOME=$MAVEN_HOME
 export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker-267
 export CHIEF_HOME=$PHRESCO_HOME/workspace/tools/chief
 export WAXSIM_HOME=$PHRESCO_HOME/workspace/tools/waxsim/usr/local/bin/waxsim
-export PATH=$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PHRESCO_HOME/workspace/tools/phantomjs-1.5.0/bin:$PATH
+export IOS_SIM_HOME=$PHRESCO_HOME/workspace/tools/ios-sim/build/Release
+export PATH=$IOS_SIM_HOME:$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PHRESCO_HOME/workspace/tools/phantomjs-1.5.0/bin:$PATH
 
