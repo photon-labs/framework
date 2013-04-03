bfafad83cecb8c2a290ea585989a15d71600f25b
surendar-r <surendar.rukmangadan@photonifotech.net>
2013-04-03 12:52:48 +0530

    jmeter home added

--- phresco-framework-runner/delivery/bin/start-framework-server.sh
+++ phresco-framework-runner/delivery/bin/start-framework-server.sh
@@ -1,5 +1,6 @@
 export PHRESCO_HOME=$PWD/..
 export JENKINS_HOME=$PHRESCO_HOME/workspace/tools/jenkins
+export JMETER_HOME=$PHRESCO_HOME/workspace/tools/apache-jmeter-2.9
 export MAVEN_HOME=$PHRESCO_HOME/tools/maven
 export SONAR_HOME=$PHRESCO_HOME/workspace/tools/sonar-3.1.1
 export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker
@@ -8,5 +9,5 @@ export M2_HOME=$MAVEN_HOME
 export WAXSIM_HOME=$PHRESCO_HOME/workspace/tools/waxsim/usr/local/bin/waxsim
 export PATH=$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
 export OCUNIT2JUNIT_HOME=$PHRESCO_HOME/workspace/tools/ocunit2junit/ocunit2junit.rb
-export PATH=$OCUNIT2JUNIT_HOME:$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
+export PATH=$OCUNIT2JUNIT_HOME:$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$JMETER_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
 mvn clean validate
