3ec539fcbf54cf6780d2307c16534142baadeb89
kaleeswaran-s <kaleeswaran.subbaiya@photoninfotech.net>
2013-04-12 18:34:40 +0530

    [artf665821] Framework-2.2:Macosx target should display in code validation pop up for MACOSx technology

---
+++ phresco-framework-impl/src/main/java/com/photon/phresco/framework/param/impl/MacosSdkParameterImpl.java
@@ -0,0 +1,51 @@
+/**
+ * Phresco Framework Implementation
+ *
+ * Copyright (C) 1999-2013 Photon Infotech Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photon.phresco.framework.param.impl;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import com.photon.phresco.api.DynamicParameter;
+import com.photon.phresco.exception.ConfigurationException;
+import com.photon.phresco.exception.PhrescoException;
+import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues;
+import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues.Value;
+import com.photon.phresco.util.IosSdkUtil;
+import com.photon.phresco.util.IosSdkUtil.MacSdkType;
+
+public class MacosSdkParameterImpl implements DynamicParameter {
+
+	public PossibleValues getValues(Map<String, Object> map) throws IOException, ParserConfigurationException, SAXException, ConfigurationException, PhrescoException {
+		PossibleValues possibleValues = new PossibleValues();
+		List<String> macosSdks = IosSdkUtil.getMacSdks(MacSdkType.macosx);
+		for (String macosSdk : macosSdks) {
+				Value value = new Value();
+				value.setValue(macosSdk);
+				possibleValues.getValue().add(value);
+		}
+		
+		return possibleValues;
+	}
+
+}
+
