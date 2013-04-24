3998858cfa1619138a8c4c5d102da8def2942d13
mohamed-as <mdasiqraja@gmail.com>
2013-04-24 20:30:07 +0530

    [artf663024] Error Response, unit test cases , Build constants implementation.

---
+++ phresco-framework-web/src/main/java/com/photon/phresco/framework/rest/api/util/BuildServiceConstants.java
@@ -0,0 +1,22 @@
+package com.photon.phresco.framework.rest.api.util;
+
+public interface BuildServiceConstants {
+	
+	
+	
+	public String APP_ID = "appId";
+	public String PROJECT_ID = "projectId";
+	public String CUSTOMER_ID = "customerId";
+	public String SELECTED_FILES ="selectedFiles";
+	
+	public String CONNECT_ACK_CHANNEL="/service/connect";
+	public String DISCONNECT_ACK_CHANNEL="/service/disconnect";
+	public String ERROR_CHANNEL="/service/error";
+	public String DATA_CHANNEL ="/service/phresco";
+	
+	public String CONNECT_ACK_MSG = "Server received your request and started processing... Pls wait!";
+	public String DISCONNECT_ACK_MSG = "Data transfer done hence Disconnecting";
+	public String DATA_FORMAT ="hh:mm:ss";
+	public String USERNAME ="username";
+
+}
