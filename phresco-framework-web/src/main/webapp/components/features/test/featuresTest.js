ab64974c1640363b7ecc97a703425a21d443eb16
arunkumar_ve <arunkumar.velusamy@photoninfotech.net>
2013-05-06 19:40:52 +0530

    [artf676549] feature on/off button

--- phresco-framework-web/src/main/webapp/components/features/test/featuresTest.js
+++ phresco-framework-web/src/main/webapp/components/features/test/featuresTest.js
@@ -1,4 +1,4 @@
-define(["jquery", "features/features", "framework/navigationController", "framework/widgetWithTemplate"], function($, Features) {
+define(["jquery", "features/features", "framework/navigationController", "framework/widgetWithTemplate", , "features/listener/featuresListener"], function($, Features) {
 	/**
 	 * Test that the setMainContent method sets the text in the MyCart-widget
 	 */
@@ -17,6 +17,21 @@ define(["jquery", "features/features", "framework/navigationController", "framew
 			features = new Features();
 			Clazz.navigationController.jQueryContainer = $("<div id='featuresTest'></div>");
 			Clazz.navigationController.push(features, false);
+			
+			setTimeout(function() {
+				output = $(Clazz.navigationController.jQueryContainer).find("#featureContent").attr('id');
+				equal("featureContent", output, "Features Rendered Successfully");
+				start();
+			}, 2000);
+			
+		});
+
+		asyncTest("Features Test", function() {
+		
+			var features, output;
+			Clazz.config = configData;
+			var featuresListener = new Clazz.com.components.features.js.listener.FeaturesListener();
+			console.info("test", featuresListener.search("Weather", "module"));
 			
 			setTimeout(function() {
 				output = $(Clazz.navigationController.jQueryContainer).find("#featureContent").attr('id');
