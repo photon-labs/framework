28e0461db790a305739226c5f89545b9d1ee6ee7
saravanan-va <saravanan.varadarajan@photoninfotech.net>
2013-05-14 12:03:05 +0530

    [ artf661981] moved to old changes

--- phresco-framework-web/src/main/java/com/photon/phresco/framework/rest/api/TechnologyService.java
+++ phresco-framework-web/src/main/java/com/photon/phresco/framework/rest/api/TechnologyService.java
@@ -13,8 +13,15 @@ import com.photon.phresco.commons.model.ApplicationType;
 import com.photon.phresco.commons.model.Customer;
 import com.photon.phresco.commons.model.TechnologyGroup;
 import com.photon.phresco.exception.PhrescoException;
+import com.photon.phresco.framework.FrameworkConfiguration;
+import com.photon.phresco.framework.PhrescoFrameworkFactory;
 import com.photon.phresco.service.client.api.ServiceManager;
+import com.photon.phresco.service.client.impl.ClientHelper;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.ClientResponse.Status;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
 
 @Path("/technology")
 public class TechnologyService {
@@ -44,12 +51,14 @@ public class TechnologyService {
 	public Response listApptypeTechInfo(@QueryParam("customerName") String customerName, @QueryParam("userId") String userId) {
 		ResponseInfo responseData = new ResponseInfo();
 		try {
-			ServiceManager serviceManager = ServiceManagerMap.CONTEXT_MANAGER_MAP.get(userId);
-			if(serviceManager == null) {
-				ResponseInfo finalOutput = ServiceManagerMap.responseDataEvaluation(responseData, null, "UnAuthorized User", null);
-				return Response.status(Status.BAD_REQUEST).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
-			}
-			Customer customer = serviceManager.getCustomerByName(customerName);
+			Client client = ClientHelper.createClient();
+			FrameworkConfiguration configuration = PhrescoFrameworkFactory.getFrameworkConfig();
+	        WebResource resource = client.resource(configuration.getServerPath() + "/admin/customers");
+	        resource = resource.queryParam("customerName", customerName);
+	        resource.accept(MediaType.APPLICATION_JSON);
+	        ClientResponse response = resource.get(ClientResponse.class);
+	        GenericType<Customer> genericType = new GenericType<Customer>() {};
+	        Customer customer = response.getEntity(genericType);
 			ResponseInfo finalOutput = ServiceManagerMap.responseDataEvaluation(responseData, null, "customer returned successfully", customer);
 			return Response.status(Status.OK).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
 			
