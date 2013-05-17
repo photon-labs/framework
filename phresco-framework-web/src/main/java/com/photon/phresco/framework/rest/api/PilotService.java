76b42df5c7401bbf6a1d5eec7a0fae79da6027a2
suresh-ma <suresh.marimuthu@photoninfotech.net>
2013-05-17 20:28:58 +0530

    service for getserver,database, webservice against pilot project

--- phresco-framework-web/src/main/java/com/photon/phresco/framework/rest/api/PilotService.java
+++ phresco-framework-web/src/main/java/com/photon/phresco/framework/rest/api/PilotService.java
@@ -10,7 +10,9 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.photon.phresco.commons.FrameworkConstants;
 import com.photon.phresco.commons.model.ApplicationInfo;
+import com.photon.phresco.commons.model.ArtifactGroupInfo;
 import com.photon.phresco.exception.PhrescoException;
 import com.photon.phresco.service.client.api.ServiceManager;
 import com.sun.jersey.api.client.ClientResponse.Status;
@@ -40,5 +42,34 @@ public class PilotService extends RestBase {
 			ResponseInfo<List<ApplicationInfo>> finalOutput = responseDataEvaluation(responseData, e, "Application pilot list not fetched", null);
 			return Response.status(Status.BAD_REQUEST).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
 		}
+	}
+	
+	@GET
+	@Path("/dependentPilot")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getDependentPilot(@QueryParam("userId") String userId, @QueryParam("pilotId") String pilotId, @QueryParam("type") String type) throws PhrescoException {
+		ResponseInfo<List<ArtifactGroupInfo>> responseData = new ResponseInfo<List<ArtifactGroupInfo>>();
+		try {
+			ServiceManager serviceManager = ServiceManagerMap.getServiceManager(userId);
+			ApplicationInfo appInfo = serviceManager.getPilotFromId(pilotId);
+			if(FrameworkConstants.REQ_SERVERS.equals(type)) {
+				List<ArtifactGroupInfo> selectedServers = appInfo.getSelectedServers();
+				ResponseInfo<List<ArtifactGroupInfo>> finalOutput = responseDataEvaluation(responseData, null, "Application pilot listed successfully", selectedServers);
+				return Response.status(Status.OK).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
+			} else if(FrameworkConstants.REQ_DATABASES.equals(type)) {
+				List<ArtifactGroupInfo> selectedDatabases = appInfo.getSelectedDatabases();
+				ResponseInfo<List<ArtifactGroupInfo>> finalOutput = responseDataEvaluation(responseData, null, "Application pilot listed successfully", selectedDatabases);
+				return Response.status(Status.OK).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
+			} else if(FrameworkConstants.REQ_WEBSERVICES.equals(type)) {
+				List<String> selectedWebservices = appInfo.getSelectedWebservices();
+				ResponseInfo<List<String>> finalOutput = responseDataEvaluation(responseData, null, "Application pilot listed successfully", selectedWebservices);
+				return Response.status(Status.OK).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
+			}
+		} catch (IOException e) {
+			ResponseInfo<List<ArtifactGroupInfo>> finalOutput = responseDataEvaluation(responseData, e, "Application pilot list not fetched", null);
+			return Response.status(Status.BAD_REQUEST).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
+		}
+		ResponseInfo<List<ArtifactGroupInfo>> finalOutput = responseDataEvaluation(responseData, null, "Application pilot list not fetched", null);
+		return Response.status(Status.BAD_REQUEST).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
 	}
 }
