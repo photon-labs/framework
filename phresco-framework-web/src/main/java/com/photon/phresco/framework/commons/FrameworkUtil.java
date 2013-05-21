21e7dc3f0c741d9cec842151c9fdfb781ca41d29
rajeshkumar-ra <rajeshkumar.ravichandra@photoninfotech.net>
2013-05-21 18:41:04 +0530

    artf687798: Unable to to functional test for device build in IOS

--- phresco-framework-web/src/main/java/com/photon/phresco/framework/commons/FrameworkUtil.java
+++ phresco-framework-web/src/main/java/com/photon/phresco/framework/commons/FrameworkUtil.java
@@ -18,34 +18,27 @@
 package com.photon.phresco.framework.commons;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.lang.reflect.Type;
 import java.math.BigDecimal;
-
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -76,7 +69,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.w3c.dom.Element;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.photon.phresco.commons.FrameworkConstants;
 import com.photon.phresco.commons.model.ApplicationInfo;
 import com.photon.phresco.commons.model.ArtifactGroup;
@@ -826,7 +818,8 @@ public class FrameworkUtil extends FrameworkBaseAction implements Constants {
     	String comma = "";
     	for (Object value : values) {
 			if (value instanceof Value) {
-				if (StringUtils.isNotEmpty(((Value) value).getDependency()) && !((Value) value).getKey().equals(((Value) currentValue).getKey()) 
+				if (StringUtils.isNotEmpty(((Value) value).getDependency()) && StringUtils.isNotEmpty(((Value) value).getKey()) &&  
+						StringUtils.isNotEmpty(((Value) currentValue).getKey()) && !((Value) value).getKey().equals(((Value) currentValue).getKey()) 
 						&& !((Value) value).getDependency().equals(((Value) currentValue).getDependency())) {
 					sb.append(comma);
 					sb.append(((Value) value).getDependency());
