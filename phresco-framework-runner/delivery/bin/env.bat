07650f31e2d4034a0de144cc44222c0caa7b1dd6
surendar-r <surendar.rukmangadan@photonifotech.net>
2013-07-10 12:32:18 +0530

    removed jmeter path url

--- phresco-framework-runner/delivery/bin/env.bat
+++ phresco-framework-runner/delivery/bin/env.bat
@@ -22,6 +22,5 @@ if "%PHRESCO_HOME%" == "%cd%\.." goto :eof
 set PHRESCO_HOME=%CD%\..
 set MAVEN_HOME=%CD%\..\tools\maven
 set JENKINS_HOME=%PHRESCO_HOME%\workspace\tools\jenkins
-set JMETER_HOME=%PHRESCO_HOME%\workspace\tools\apache-jmeter-2.9
 set M2_HOME=%MAVEN_HOME%
 set PATH=%MAVEN_HOME%\bin;%PHRESCO_HOME%\bin;%PHRESCO_HOME%\workspace\tools\phantomjs-1.5.0-win32-static;%PATH%;
