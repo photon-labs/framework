b2923c2c8c99cd248eb3d18aca6920b704a6b160
santhosh <santhosh.janakiram@photoninfotech.net>
2013-07-25 12:19:57 +0530

    [artf719970] UnitTest case for WarProjectModuleImpl, IosAndMacosSdkParameterImpl, EnvironmentWebservicesImpl, EnvironmentServersImpl classes

--- phresco-framework-impl/src/test/java/com/photon/phresco/framework/param/impl/AllTest.java
+++ phresco-framework-impl/src/test/java/com/photon/phresco/framework/param/impl/AllTest.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.photon.phresco.framework.impl.ConfigurationReaderTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MacosSdkParameterImplTest.class, IosSdkParameterImplTest.class, IosSimSDKVersionsParameterImplTest.class, ConfigurationReaderTest.class, EnvironmentDatabaseImplTest.class, TechnologyVersionImplTest.class, ProjectModuleImplTest.class})
+@SuiteClasses({ MacosSdkParameterImplTest.class, IosSdkParameterImplTest.class, IosSimSDKVersionsParameterImplTest.class, ConfigurationReaderTest.class, EnvironmentDatabaseImplTest.class, TechnologyVersionImplTest.class, ProjectModuleImplTest.class, WarProjectModuleImplTest.class, IosAndMacosSdkParameterImplTest.class, EnvironmentWebservicesImplTest.class, EnvironmentServersImplTest.class})
 
 public class AllTest {
 	// intentionally blank. All tests were added via annotations
