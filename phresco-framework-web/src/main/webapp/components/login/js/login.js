01099e8f9e520b8380ba80d709c6427a6852318e
sathiyaselvan-s <sathiyaselvan.subramaniyan@photoninfotech.net>
2013-07-31 18:09:22 +0530

    Fixed Issues in add and edit project date issues.

--- phresco-framework-web/src/main/webapp/components/login/js/login.js
+++ phresco-framework-web/src/main/webapp/components/login/js/login.js
@@ -122,6 +122,12 @@ define(["framework/widgetWithTemplate", "login/listener/loginListener"], functio
 			
 			$('#username').focus();
 			Clazz.navigationController.jQueryContainer = commonVariables.contentPlaceholder;
+			
+			$('#password').keypress(function(e) {
+				if (e.which === 32) {
+					e.preventDefault();
+				} 
+			});
 		}
 	});
 
