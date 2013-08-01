c0b88554f3c909d7f54a16c9550bacb66d00adbb
surendar-r <surendar.rukmangadan@photonifotech.net>
2013-08-01 17:14:55 +0530

    added new phresco-install file

---
+++ phresco-framework-runner/delivery/bin/phresco-install.sh
@@ -0,0 +1,44 @@
+#
+# Framework Runner
+#
+# Copyright (C) 1999-2013 Photon Infotech Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+export JENKINS_HOME=$PHRESCO_HOME/workspace/tools/jenkins
+export JMETER_HOME=$PHRESCO_HOME/workspace/tools/apache-jmeter-2.9
+export MAVEN_HOME=$PHRESCO_HOME/tools/maven
+export SONAR_HOME=$PHRESCO_HOME/workspace/tools/sonar-3.1.1
+export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker
+export CHIEF_HOME=$PHRESCO_HOME/workspace/tools/chief
+export M2_HOME=$MAVEN_HOME
+export WAXSIM_HOME=$PHRESCO_HOME/workspace/tools/waxsim/usr/local/bin/waxsim
+export PATH=$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
+export OCUNIT2JUNIT_HOME=$PHRESCO_HOME/workspace/tools/ocunit2junit/ocunit2junit.rb
+export PATH=$OCUNIT2JUNIT_HOME:$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$JMETER_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
+cd $PHRESCO_HOME/bin
+
+NAME=phresco
+
+START_DAEMON="mvn clean validate"
+
+case "$1" in
+    console)
+        $START_DAEMON
+        ;;
+
+*)
+echo "Usage: $NAME { console }" >&2
+        exit 1
+        ;;
+esac
