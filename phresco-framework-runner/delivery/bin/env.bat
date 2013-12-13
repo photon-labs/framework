e1c9b794bc80f93e7df86ddf11d01aa299815efc
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2013-12-13 20:35:33 +0530

    tfs dependency added

--- phresco-framework-runner/delivery/bin/env.bat
+++ phresco-framework-runner/delivery/bin/env.bat
@@ -23,4 +23,5 @@ set PHRESCO_HOME=%CD%\..
 set MAVEN_HOME=%CD%\..\tools\maven
 set JENKINS_HOME=%PHRESCO_HOME%\workspace\tools\jenkins
 set M2_HOME=%MAVEN_HOME%
-set PATH=%MAVEN_HOME%\bin;%PHRESCO_HOME%\bin;%PHRESCO_HOME%\workspace\tools\phantomjs-1.5.0-win32-static;%PATH%;
+set TFS_HOME=%PHRESCO_HOME%\workspace\tools\native\native
+set PATH=%MAVEN_HOME%\bin;%PHRESCO_HOME%\bin;%PHRESCO_HOME%\workspace\tools\phantomjs-1.5.0-win32-static;%TFS_HOME%;%PATH%;
