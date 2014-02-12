5a535cf2c62003248b7f73a386b51e4dc631ad59
saravanan-na <saravanan.natarajan@photoninfotech.net>
2014-02-12 18:23:21 +0530

    Publications.java file added

---
+++ phresco-framework-web/src/main/java/com/photon/phresco/framework/model/Publications.java
@@ -0,0 +1,37 @@
+package com.photon.phresco.framework.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@XmlRootElement
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Publications {
+
+	private String id;
+	private String item;
+	
+	public Publications() {
+
+	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	@Override
+	public String toString() {
+		return "Publications [id=" + id + ", item=" + item + "]";
+	}
+}
