22509e1630929a8cd7c9187836089c63f997a68b
kumar-s <kumar.selvaraj@photoninfotech.net>
2014-04-03 19:17:10 +0530

    [artf947522] Unwanted library files removed from phresco modules

--- phresco-framework-web/src/test/java/com/photon/phresco/framework/rest/api/LiquibaseServiceTest.java
+++ phresco-framework-web/src/test/java/com/photon/phresco/framework/rest/api/LiquibaseServiceTest.java
@@ -32,7 +32,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.easymock.PowerMock;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
