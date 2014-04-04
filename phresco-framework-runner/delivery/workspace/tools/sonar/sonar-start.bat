bf1126a8abe837f62a3d86370ca8decbc11fa74a
surendar-r <surendar.rukmangadan@photonifotech.net>
2014-04-04 16:05:37 +0530

    commting the files for sonar as a seperate tool

--- phresco-framework-runner/delivery/workspace/tools/sonar/sonar-start.bat
+++ phresco-framework-runner/delivery/workspace/tools/sonar/sonar-start.bat
@@ -2,4 +2,5 @@
 echo "before env.bat stating sonar"
 call env.bat
 echo "stating sonar"
+mvn clean install
 mvn t7:run-forked
--- phresco-framework-runner/delivery/workspace/tools/sonar/sonar-start.sh
+++ phresco-framework-runner/delivery/workspace/tools/sonar/sonar-start.sh
@@ -1,10 +1,11 @@
-@echo off
-export PHRESCO_HOME=$PWD\..\..\..
-export SONAR_HOME=$PWD\..\sonar-3.5.1
-export MAVEN_HOME=$PWD\..\..\..\tools\maven
-export JENKINS_HOME=$PHRESCO_HOME\workspace\tools\jenkins
+
+export PHRESCO_HOME=$PWD/../../..
+export SONAR_HOME=$PWD/../sonar-3.5.1
+export MAVEN_HOME=$PWD/../../../tools/maven
+export JENKINS_HOME=$PHRESCO_HOME/workspace/tools/jenkins
 export M2_HOME=$MAVEN_HOME
-export TFS_HOME=$PHRESCO_HOME\workspace\tools\native\native
-export PATH=$MAVEN_HOME\bin:$PHRESCO_HOME\bin:$PHRESCO_HOME/workspace/tools/phantomjs-1.5.0-mac/bin:$TFS_HOME:$PATH
+export TFS_HOME=$PHRESCO_HOME/workspace/tools/native/native
+export PATH=$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PHRESCO_HOME/workspace/tools/phantomjs-1.5.0-mac/bin:$TFS_HOME:$PATH
 echo "stating sonar"
+
 mvn t7:run-forked
