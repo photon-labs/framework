5667f605968112dc095eeb082653bee6de8091ae
mahesh <mahesh.prathapaneni@photoninfotech.net>
2014-04-08 16:37:31 +0530

    added sonar setup files

--- phresco-framework-runner/delivery/workspace/tools/sonar/sonar-setup.sh
+++ phresco-framework-runner/delivery/workspace/tools/sonar/sonar-setup.sh
@@ -6,6 +6,5 @@ export JENKINS_HOME=$PHRESCO_HOME/workspace/tools/jenkins
 export M2_HOME=$MAVEN_HOME
 export TFS_HOME=$PHRESCO_HOME/workspace/tools/native/native
 export PATH=$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PHRESCO_HOME/workspace/tools/phantomjs-1.5.0-mac/bin:$TFS_HOME:$PATH
-echo "starting sonar"
-
-mvn t7:run-forked
+echo "stetting up sonar"
+mvn clean install
--- phresco-framework-runner/delivery/workspace/tools/sonar/sonar-start.bat
+++ phresco-framework-runner/delivery/workspace/tools/sonar/sonar-start.bat
@@ -1,6 +1,4 @@
 @echo off
-echo "before env.bat stating sonar"
 call env.bat
 echo "stating sonar"
-mvn clean install
 mvn t7:run-forked
