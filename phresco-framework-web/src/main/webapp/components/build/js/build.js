1f160682cd8c4b3c83afcd533ada9f21dee9e029
viji <viji.ramasamy@photoninfotech.net>
2014-04-15 19:27:59 +0530

    bug fixed

--- phresco-framework-web/src/main/webapp/components/build/js/build.js
+++ phresco-framework-web/src/main/webapp/components/build/js/build.js
@@ -247,10 +247,7 @@ define(["build/listener/buildListener"], function() {
 						buildObject.buildInfos = response.data;
 						buildObject.userPermissions = userPermissions;
 						
-						if($("#buildRow").length < 1){
-							var table = '<table class="table table-striped table_border table-bordered big" cellpadding="0" cellspacing="0" border="0" id="buildRow"><thead class="fixedHeader"><tr><th data-i18n="build.label.bNo"></th><th data-i18n="build.label.date"></th><th data-i18n="build.label.download"></th><th name="prcBuild" data-i18n="build.label.processBuild"></th><th name="buildDep" data-i18n="build.label.deploy"></th><th data-i18n="build.label.delete"></th><th data-i18n="build.label.upload"></th></tr></thead><tbody class="scrollContent"></tbody></table>';
-							$('.qual_unit_main').html(table);
-						}
+						
 
 						$.each(buildObject.buildInfos, function(index, current){
 							var cancreateIpa = "";
@@ -258,11 +255,22 @@ define(["build/listener/buildListener"], function() {
 							var deviceDeploy = "";
 							var deleteOpt = "";
 							var testFlightupload = "";
+						
+						if($("#buildRow").length < 1){	  
+						 if(current.options !== null && current.options.canCreateIpa === true){
+						   var table = '<table class="table table-striped table_border table-bordered big" cellpadding="0" cellspacing="0" border="0" id="buildRow"><thead class="fixedHeader"><tr><th data-i18n="build.label.bNo"></th><th data-i18n="build.label.date" ></th><th data-i18n="build.label.download"></th><th name="prcBuild" data-i18n="build.label.processBuild"></th><th name="buildDep" data-i18n="build.label.deploy"></th><th data-i18n="build.label.delete"></th><th data-i18n="build.label.upload"></th></tr></thead><tbody class="scrollContent"></tbody></table>';
+                          }
+						else{
+						   var table = '<table class="table table-striped table_border table-bordered big" cellpadding="0" cellspacing="0" border="0" id="buildRow"><thead class="fixedHeader"><tr><th data-i18n="build.label.bNo"></th><th data-i18n="build.label.date" ></th><th data-i18n="build.label.download"></th><th name="prcBuild" data-i18n="build.label.processBuild"></th><th name="buildDep" data-i18n="build.label.deploy"></th><th data-i18n="build.label.delete"></th></tr></thead><tbody class="scrollContent"></tbody></table>'; 
+							}
+						   $('.qual_unit_main').html(table);
+						 }
 
-							if(current.options !== null && current.options.canCreateIpa === true){
+					 if(current.options !== null && current.options.canCreateIpa === true){		     
+						
 								cancreateIpa = '<a href="javascript:void(0)" class="tooltiptop" title="" data-placement="bottom" data-toggle="tooltip" data-original-title="IPA Download"><img name="ipaDownload" src="themes/default/images/Phresco/ipa_icon.png" width="13" height="18" border="0" alt=""></a>';
 								
-								testFlightupload = '<td class="list_img" name="UploadBuildtd"><a href="javascript:void(0)" class="tooltiptop" title="" data-placement="bottom" data-toggle="tooltip" data-original-title="Upload"><img build="uploadBuild" name="upload_'+current.buildNo+'" src="themes/default/images/Phresco/testflight_icon.png" width="15" height="18" border="0" alt=""></a><div id="upload_'+current.buildNo+'" name="testFlightUploadDiv" class="dyn_popup deleteproj_msg"><div><div><font class="testflight">Api Token<sup>*</sup></font><input id="apiToken" type="text" maxlength="253" value="" name="ApiToken" showhide="true" parametertype="String"><input id="apiToken" type="hidden"  name="fileName"></div><div><font class="testflight">Team Token<sup>*</sup></font><input id="teamToken" type="text" maxlength="253" value="" name="teamToken" showhide="true" parametertype="String"></div><div><font class="testflight">Notes<sup>*</sup></font><input id="notes" type="text" maxlength="253" value="" name="notes" showhide="true" parametertype="String"></div><div><font class="testflight">Notify</font><input id="notify" type="text" maxlength="253" value="" name="notify" showhide="true" parametertype="String"></div><div><font class="testflight">Distribution Lists</font><input id="distributionLists" type="text" maxlength="253" value="" name="distributionLists" showhide="true" parametertype="String"></div><div class="testflight_btn"><input type="button" name="uploadTestFlight" fileExtn="ipa" buildNo='+current.buildNo+' data-i18n="[value]build.label.upload" class="btn btn_style" /><input type="button" data-i18n="[value]build.label.close" class="btn btn_style dyn_popup_close" /></div></div></div></td>'
+								testFlightupload = '<td class="list_img" name="UploadBuildtd"><a href="javascript:void(0)" class="tooltiptop" title="" data-placement="bottom" data-toggle="tooltip" data-original-title="Upload"><img build="uploadBuild" name="upload_'+current.buildNo+'" src="themes/default/images/Phresco/testflight_icon.png" width="15" height="18" border="0" alt=""></a><div id="upload_'+current.buildNo+'" name="testFlightUploadDiv" class="dyn_popup deleteproj_msg" style="display:none;"><div><div><font class="testflight">Api Token<sup>*</sup></font><input id="apiToken" type="text" maxlength="253" value="" name="ApiToken" showhide="true" parametertype="String"><input id="apiToken" type="hidden"  name="fileName"></div><div><font class="testflight">Team Token<sup>*</sup></font><input id="teamToken" type="text" maxlength="253" value="" name="teamToken" showhide="true" parametertype="String"></div><div><font class="testflight">Notes<sup>*</sup></font><input id="notes" type="text" maxlength="253" value="" name="notes" showhide="true" parametertype="String"></div><div><font class="testflight">Notify</font><input id="notify" type="text" maxlength="253" value="" name="notify" showhide="true" parametertype="String"></div><div><font class="testflight">Distribution Lists</font><input id="distributionLists" type="text" maxlength="253" value="" name="distributionLists" showhide="true" parametertype="String"></div><div class="testflight_btn"><input type="button" name="uploadTestFlight" fileExtn="ipa" buildNo='+current.buildNo+' data-i18n="[value]build.label.upload" class="btn btn_style" /><input type="button" data-i18n="[value]build.label.close" class="btn btn_style dyn_popup_close" /></div></div></div></td>'
 							}
 
 							deviceDeploy = '<div id="deploye_'+ current.buildNo +'" class="dyn_popup popup_bg" style="display:none;"><div id="bdeploy_'+ current.buildNo +'"><form name="deployForm"><ul class="row dynamicControls"></ul><div class="hiddenControls"></div></form><div class="flt_right"><input type="button" name="deploy" data-i18n="[value]build.label.deploy" class="btn btn_style dyn_popup_close"><input type="button" data-i18n="[value]build.label.close" class="btn btn_style dyn_popup_close"></div></div></div>';
