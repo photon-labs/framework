beea4ab678ea15b1fd085b355a2e38575f32419b
Surendar-r <surendar.rukmangadan@photoninfotech.net>
2014-05-13 17:11:23 +0530

    sonar home not recognized.

--- phresco-framework-runner/delivery/bin/env.bat
+++ phresco-framework-runner/delivery/bin/env.bat
@@ -22,6 +22,7 @@ if "%PHRESCO_HOME%" == "%cd%\.." goto :eof
 set PHRESCO_HOME=%CD%\..
 set MAVEN_HOME=%CD%\..\tools\maven
 set JENKINS_HOME=%PHRESCO_HOME%\workspace\tools\jenkins
+set SONAR_HOME=%PHRESCO_HOME%\workspace\tools\sonar-3.5.1
 set M2_HOME=%MAVEN_HOME%
 set TFS_HOME=%PHRESCO_HOME%\workspace\tools\native\native
-set PATH=%MAVEN_HOME%\bin;%PHRESCO_HOME%\bin;%PHRESCO_HOME%\workspace\tools\phantomjs-1.5.0-win32-static;%TFS_HOME%;%PATH%;
+set PATH=%SONAR_HOME%\bin;%MAVEN_HOME%\bin;%PHRESCO_HOME%\bin;%PHRESCO_HOME%\workspace\tools\phantomjs-1.5.0-win32-static;%TFS_HOME%;%PATH%;
